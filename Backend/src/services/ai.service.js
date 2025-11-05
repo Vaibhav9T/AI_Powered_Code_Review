5f8cb3c2930ea4ebfcedd78722533b1e3cbad53d
Vaibhav Annaso Tembukade <vaibhavtembukadea09@gmail.com>
2025-11-05 20:13:47 +0530

    API key added

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,6 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({apiKey: process.env.GOOGLE_GEMINI_KEY});
+
 //const model="gemini-2.5-flash";
 
 // async function main() {
@@ -99,3 +100,15 @@ async function generateContent(code){
 }
 
 export { generateContent };
+
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+await main();
+
